c1f9365f8e596dc84f1811f310b5d9a8228a2fe9
Ekansh Sharma <ekanshh.sharma@gmail.com>
2022-08-29 15:53:20 +0200

    Added waypoints and trajectory for peg insertion

--- src/my_environments/pressfit_v2.py
+++ src/my_environments/pressfit_v2.py
@@ -1,4 +1,32 @@
-from cgi import print_environ_usage
+from collections import OrderedDict
+import os
+import numpy as np
+import pandas as pd
+import re
+from klampt.model import trajectory
+import roboticstoolbox as rtb
+
+from spatialmath import SE3
+
+from robosuite.utils.transform_utils import convert_quat, quat2mat, mat2euler
+from robosuite.utils.mjcf_utils import CustomMaterial
+
+from robosuite.environments.manipulation.single_arm_env import SingleArmEnv
+
+from robosuite.models.tasks import ManipulationTask
+from robosuite.utils.placement_samplers import UniformRandomSampler
+from robosuite.utils.observables import Observable, sensor
+from robosuite.models.base import MujocoModel
+
+import robosuite.utils.transform_utils as T
+
+from my_models.objects import SoftTorsoObject, SoftBoxObject
+from my_models.tasks import UltrasoundTask
+from my_models.arenas import UltrasoundArena
+from utils.quaternion import distance_quat, difference_quat
+
+
+
 import numpy as np
 from my_models.objects.xml_objects import TetrapackObject
 
@@ -12,9 +40,8 @@ from robosuite.utils.observables import Observable, sensor
 
 
 # Python imports
-from distutils.log import warn
 import re
-from warnings import WarningMessage
+from copy import deepcopy
 import numpy as np
 from my_models.arenas.pressfit_arena import PressfitArena
 # Local imports
@@ -48,7 +75,7 @@ class PressfitV2(SingleArmEnv):
         reward_shaping=False,
         has_renderer=False,
         has_offscreen_renderer=True,
-        render_camera="customview",
+        render_camera=None,
         render_collision_mesh=True,
         render_visual_mesh=True,
         render_gpu_device_id=-1,
@@ -66,6 +93,9 @@ class PressfitV2(SingleArmEnv):
     ):
         # Assert that the gripper type is None
         # assert gripper_types is None, "Tried to specify gripper other than None in TwoArmPegInHole environment!"
+
+        # 
+        self.via_point = {}
 
         # reward configuration
         self.reward_scale = reward_scale
@@ -162,6 +192,10 @@ class PressfitV2(SingleArmEnv):
 
         # initialize objects of interest
         self.hole = ContainerWithTetrapacksObject(name="container_with_tetrapacks")
+        # Load hole object
+        hole_obj = self.hole.get_obj()
+        hole_obj.set('quat', '0.707 0. 0. 0.707')
+        hole_obj.set('pos', '0.2 0.0 1.4')
         
         # To check if the hole is correctly shown in simulation
         # self.peg_radius=(0.01, 0.03)
@@ -172,18 +206,12 @@ class PressfitV2(SingleArmEnv):
         #     size_max=(self.peg_radius[1], self.peg_length),
         #     rgba=[0, 1, 0, 1],
         #     joints=None,
+            
         # )
         # peg_obj = self.peg.get_obj()
-        # peg_obj.set('pos', '-0.242 0.059 1.585' )
+        # peg_obj.set('pos', '0.045  0.     1.63' )
+        # # peg_obj.set('pos', '0.465 0.    1.63')
         # peg_obj.set('quat', '-0.500 0.500 0.500 -0.500')
-
-        # Load hole object
-        hole_obj = self.hole.get_obj()
-        hole_obj.set('quat', '0.707 0. 0. 0.707')
-        hole_obj.set('pos', '0.2 0.0 1.4')
-
-       
-
         # Append appropriate objects to arms
 
         # task includes arena, robot, and objects of interest
@@ -208,9 +236,9 @@ class PressfitV2(SingleArmEnv):
 
         # Additional object references from this env
         self.hole_body_id = self.sim.model.body_name2id('container_with_tetrapacks_tetrapack_4')
-        print(f"Hole Body ID: {self.hole_body_id}")
+        # print(f"Hole Body ID: {self.hole_body_id}")
         self.peg_body_id = self.sim.model.body_name2id(self.robots[0].gripper.root_body)
-        print(f"Peg Body ID: {self.peg_body_id}")
+        # print(f"Peg Body ID: {self.peg_body_id}")
 
     def _setup_observables(self):
         """
@@ -226,6 +254,15 @@ class PressfitV2(SingleArmEnv):
             # Get robot prefix and define observables modality
             pf = self.robots[0].robot_model.naming_prefix
             modality = "object"
+
+            
+            @sensor(modality=modality)
+            def eef_contact_force(obs_cache):
+                return self.sim.data.cfrc_ext[self.probe_id][-3:]
+
+            @sensor(modality=modality)
+            def eef_torque(obs_cache):
+                return self.robots[0].ee_torque
 
             # position and rotation of peg and hole
             @sensor(modality=modality)
@@ -276,12 +313,12 @@ class PressfitV2(SingleArmEnv):
             names = [s.__name__ for s in sensors]
 
             # Create observables
-            for name, s in zip(names, sensors):
-                observables[name] = Observable(
-                    name=name,
-                    sensor=s,
-                    sampling_rate=self.control_freq,
-                )
+            # for name, s in zip(names, sensors):
+            #     observables[name] = Observable(
+            #         name=name,
+            #         sensor=s,
+            #         sampling_rate=self.control_freq,
+            #     )
         return observables
 
     def _reset_internal(self):
@@ -289,7 +326,36 @@ class PressfitV2(SingleArmEnv):
         Resets simulation internal configurations.
         """
         super()._reset_internal()
-        self.robots[0].set_robot_joint_positions([0.000, -1.019, -0.005, -2.498, 0.050, 2.942, 0.785])
+
+        self.robots[0].set_robot_joint_positions([0.30069401,  0.81881,    -1.22213888, -6.90838528,  1.57079512,  5.74327216, -0.13926924]) 
+
+        self.trajectory = self.get_trajectory()  # get the scanning trajectory(by JadeCong)
+        self.initial_traj_step = 0  # np.random.default_rng().uniform(low=0, high=self.num_waypoints - 1)  # set to zero(by JadeCong)
+        self.traj_step = self.initial_traj_step  # step at which to evaluate trajectory. Must be in interval [0, num_waypoints - 1]
+            
+        self.traj_pt = self.trajectory.eval(self.traj_step)[1]  # (by JadeCong)
+        print(self.traj_pt)
+
+        self._add_waypoint_site(self.viewer.viewer, "TP1", origin_size=[0.01, 0.01, 0.01], axis_size=[0.005, 0.005, 0.005],
+                                pos=self.traj_pt, quat=np.array([0, 0, 0, 1]))
+        
+        self.traj_ori = T.quat2axisangle(T.mat2quat(np.array(self.trajectory.eval(self.traj_step)[0]).reshape(3, 3)))  # (by JadeCong)
+        self.traj_pt_vel = self.trajectory.deriv(self.traj_step)[1]  # (by JadeCong)
+
+        # initialize controller's trajectory
+        self.robots[0].controller.traj_pos = self.traj_pt
+        self.robots[0].controller.traj_ori = self.traj_ori  # (by JadeCong)
+
+        # get initial joint positions for robot
+        init_qpos = self._get_initial_qpos()
+
+        print(f"Init_qpos from trajectory generation: {init_qpos}")
+
+        # override initial robot joint positions
+        self.robots[0].set_robot_joint_positions(init_qpos)
+
+        # update controller with new initial joints
+        self.robots[0].controller.update_initial_joints(init_qpos)  
 
     def _check_success(self):
         """
@@ -337,7 +403,27 @@ class PressfitV2(SingleArmEnv):
             d,
             abs(np.dot(hole_normal, v) / np.linalg.norm(hole_normal) / np.linalg.norm(v)),
         )
+    
+    def _post_action(self, action):
+       
+        reward, done, info = super()._post_action(action)   
 
+       
+        self._add_waypoint_site(self.viewer.viewer, "EEF1", origin_size=[0.01, 0.01, 0.01], axis_size=[0.005, 0.005, 0.005],
+                                pos=self._eef_xpos, quat=np.array([0, 0, 0, 1]))
+
+        self._add_waypoint_site(self.viewer.viewer, "WP1", origin_size=[0.01, 0.01, 0.01], axis_size=[0.005, 0.005, 0.005],
+                                pos=self.via_point['p0'], quat=np.array([0, 0, 0, 1]))
+
+        self._add_waypoint_site(self.viewer.viewer, "WP2", origin_size=[0.01, 0.01, 0.01], axis_size=[0.005, 0.005, 0.005],
+                                pos=self.via_point['p1'], quat=np.array([0, 0, 0, 1]))
+
+        self._add_waypoint_line(self.viewer.viewer, "", rgba=[0, 1, 0.7, 1], pos_start=self.via_point['p0'], pos_end=self.via_point['p1'])
+
+
+        return reward, done, info
+
+    
     def _peg_pose_in_hole_frame(self):
         """
         A helper function that takes in a named data field and returns the pose of that
@@ -360,3 +446,183 @@ class PressfitV2(SingleArmEnv):
 
         peg_pose_in_hole = T.pose_in_A_to_pose_in_B(peg_pose_in_world, world_pose_in_hole)
         return peg_pose_in_hole
+
+    def get_waypoint(self, ):
+
+        self.peg_length = 0.14   # Hard-coded, see the tetrapack xml
+
+        temp_distance = 2 * self.peg_length   
+        pos_via_point_0 = deepcopy(self.sim.data.get_site_xpos('container_with_tetrapacks_tetrapack_4'))
+        # print(pos_via_point_0)
+        pos_via_point_0[0] -= temp_distance     
+        pos_via_point_0[2] += 0.02
+        pos_via_point_1 = deepcopy(self.sim.data.get_site_xpos('container_with_tetrapacks_tetrapack_4'))
+        pos_via_point_1[0] += 1* self.peg_length 
+        hole_angle = self.sim.data.get_body_xquat('container_with_tetrapacks_tetrapack_4')
+        angle_desired = [0.5, 0.5, 0.5, 0.5]
+        print(f"Goal Quat: {angle_desired}")
+        # TODO: Induce some noise to the starting position
+        trans_error = [0, 0, 0]
+
+        self.via_point['p0'] = pos_via_point_0 + trans_error
+        self.via_point['o0'] = angle_desired
+        self.via_point['p1'] = pos_via_point_1 + trans_error
+        self.via_point['o1'] = angle_desired
+
+    def get_trajectory(self):
+        """
+        Calculates a trajectory between two waypoints on the torso. The waypoints are extracted from a grid on the torso.
+        The first waypoint is given at time t=0, and the second waypoint is given at t=1.
+        If self.deterministic_trajectory is true, a deterministic trajectory along the x-axis of the torso is calculated.
+
+        Args:
+
+        Returns:
+            (klampt.model.trajectory Object):  trajectory
+        """
+        waypoints = self.get_waypoint()  
+
+        start_point = self.via_point['p0']
+        end_point = self.via_point['p1']
+        goal_quat = self.via_point['o1']
+        quat_array = quat2mat(goal_quat).flatten()
+        waypoints = np.array([np.append(quat_array, start_point), np.append(quat_array, end_point)])  # R+t(9+3)
+
+        self.traj_start = waypoints[0][9:]
+        self.traj_end = waypoints[-1][9:]
+        milestones = np.array(waypoints)
+
+        self.num_waypoints = np.size(milestones, 0)
+
+        return trajectory.SE3Trajectory(milestones=milestones)
+
+    def _convert_robosuite_to_toolbox_xpos(self, pos):
+        """
+        Converts origin used in robosuite to origin used for robotics toolbox. Also transforms robosuite world frame (vectors x, y, z) to
+        to correspond to world frame used in toolbox.
+        Args:
+            pos (np.array): position (x,y,z) given in robosuite coordinates and frame
+        Returns:
+            (np.array):  position (x,y,z) given in robotics toolbox coordinates and frame
+        """
+        xpos_offset = self.robots[0].robot_model.base_xpos_offset["table"](self.table_full_size[0])[0]
+        zpos_offset = self.robots[0].robot_model.top_offset[-1] - 0.016
+
+        # the numeric offset values have been found empirically, where they are chosen so that 
+        # self._eef_xpos matches the desired position.
+        if self.robots[0].name == "UR5e":
+            return np.array([-pos[0] + xpos_offset + 0.08, -pos[1] + 0.025, pos[2] - zpos_offset + 0.15]) 
+
+        if self.robots[0].name == "Panda":
+            return np.array([pos[0] - xpos_offset - 0.06, pos[1], pos[2] - zpos_offset + 0.111])
+            
+    def _get_initial_qpos(self):
+        """
+        Calculates the initial joint position for the robot based on the initial desired pose (self.traj_pt, self.goal_quat).
+        If self.initial_probe_pos_randomization is True, Guassian noise is added to the initial position of the probe.
+        Args:
+        Returns:
+            (np.array): n joint positions 
+        """
+        pos = np.array(self.traj_pt)
+
+        pos = self._convert_robosuite_to_toolbox_xpos(pos)
+        # ori_euler = mat2euler(quat2mat(self.goal_quat))
+        ori_euler = mat2euler(np.array(self.trajectory.eval(self.traj_step)[0]).reshape(3, 3))  # (by JadeCong)
+
+        # desired pose
+        T = SE3(pos) * SE3.RPY(ori_euler)
+
+        # find initial joint positions
+        if self.robots[0].name == "UR5e":
+            robot = rtb.models.DH.UR5()
+            sol = robot.ikine_min(T, q0=self.robots[0].init_qpos)
+            # sol = robot.ikine_min(T, q0=self.robots[0]._joint_positions)  # by JadeCong
+
+            # flip last joint around (pi)
+            sol.q[-1] -= np.pi
+            return sol.q
+
+        elif self.robots[0].name == "Panda":
+            robot = rtb.models.DH.Panda()
+            sol = robot.ikine_min(T, q0=self.robots[0].init_qpos)
+            # sol = robot.ikine_min(T, q0=self.robots[0]._joint_positions)  # by JadeCong
+            return sol.q
+
+    #################################################################
+    # To add markers
+    #################################################################
+    
+    def _add_marker(self, viewer, name="mkr", origin_size=[0.006, 0.006, 0.006], pos=[0, 0, 0], quat=[1, 0, 0, 0]):
+        # Import the dependencies
+        import numpy as np
+        from scipy.spatial.transform import Rotation as R
+
+        # Add the origin of the marker
+        viewer.add_marker(type=2,  # type=const.GEOM_SPHERE,  # 2 for sphere
+                          size=origin_size,
+                          pos=np.array(pos),
+                          mat=R.from_quat(quat[[0, 1, 2, 3]]).as_matrix().flatten(),
+                          # transfer the index from wxyz to xyzw
+                          rgba=[1, 1, 0, 1],
+                          label=name)
+
+    def _add_waypoint_site(self, viewer, name="wp", origin_size=[0.006, 0.006, 0.006], axis_size=[0.002, 0.002, 0.2],
+                            pos=[0, 0, 0], quat=[1, 0, 0, 0]):
+            # Import the dependencies
+            import numpy as np
+            from scipy.spatial.transform import Rotation as R
+
+            # print("inside")
+            # Add the origin of the site
+            viewer.add_marker(type=2,  # type=const.GEOM_SPHERE,  # 2 for sphere
+                            size=origin_size,
+                            pos=np.array(pos),
+                            mat=R.from_quat(quat[[0, 1, 2, 3]]).as_matrix().flatten(),  # transfer the index from wxyz to xyzw
+                            rgba=[1, 1, 0, 1],
+                            label=name)
+
+            # Add the XYZ axes of the site
+            viewer.add_marker(type=100,  # type=const.GEOM_ARROW,  # 5 for cylinder and 100 for arrow
+                            size=axis_size,
+                            pos=np.array(pos),
+                            mat=(R.from_quat(quat[[0, 1, 2, 3]]) * R.from_euler('xyz', [0, 90, 0], degrees=True)).as_matrix().flatten(),  # transfer the index from wxyz to xyzw
+                            rgba=[1, 0, 0, 1],
+                            label="")
+            viewer.add_marker(type=100,  # type=const.GEOM_ARROW,  # 5 for cylinder and 100 for arrow
+                            size=axis_size,
+                            pos=np.array(pos),
+                            mat=(R.from_quat(quat[[0, 1, 2, 3]]) * R.from_euler('xyz', [-90, 0, 0], degrees=True)).as_matrix().flatten(),  # transfer the index from wxyz to xyzw
+                            rgba=[0, 1, 0, 1],
+                            label="")
+            viewer.add_marker(type=100,  # type=const.GEOM_ARROW,  # 5 for cylinder and 100 for arrow
+                            size=axis_size,
+                            pos=np.array(pos),
+                            mat=(R.from_quat(quat[[0, 1, 2, 3]]) * R.from_euler('xyz', [0, 0, 0], degrees=True)).as_matrix().flatten(),  # transfer the index from wxyz to xyzw
+                            rgba=[0, 0, 1, 1],
+                            label="")
+
+    def _add_waypoint_line(self, viewer, name="line", rgba=[0, 1, 0.5, 1], pos_start=[0, 0, 0], pos_end=[1, 1, 1]):
+
+        # Import the dependencies
+        import numpy as np
+        from scipy.spatial.transform import Rotation as R
+
+        # Function for getting the rotation vector between two vectors
+        def get_rotation_vector(vector1, vector2):  # rotation from vector1 to vector2
+            # get the rotation axis
+            rot_axis = np.cross(vector1, vector2)
+            # get the rotation angle
+            rot_angle = np.arccos(vector1.dot(vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2)))
+            # get the rotation vector
+            rot_vector = rot_axis / np.linalg.norm(rot_axis) * rot_angle
+
+            return rot_vector
+
+        # Add the line between two waypoints
+        viewer.add_marker(type=103,  # type=const.GEOM_LINE,  # 103 for line
+                          size=[1, 1, np.linalg.norm(pos_end - pos_start)],
+                          pos=pos_start,  # the pos of start waypoint
+                          mat=R.from_rotvec(get_rotation_vector(np.array([0, 0, 1]), (pos_end - pos_start))).as_matrix().flatten(),  # transfer the index from wxyz to xyzw
+                          rgba=rgba,
+                          label=name)
--- src/test_pressfit_v2.py
+++ src/test_pressfit_v2.py
@@ -1,29 +1,62 @@
 import numpy as np
 from numpy import True_
+from sympy import false
 from my_environments import PressfitV2
 from my_models.grippers.tetrapack_gripper import TetrapackGripper
 import robosuite as suite
 from robosuite.environments.base import register_env
 from robosuite.models import MujocoWorldBase
 from utils.common import register_gripper
+from copy import deepcopy
+import robosuite.utils.transform_utils as T
 
 register_env(PressfitV2)
 register_gripper(TetrapackGripper)
+from robosuite import load_controller_config
+
+# def calc_action(observation, env):
+#     target_pos = [-9.49949353e-02, -1.40693069e-06,  1.63983968e+00] 
+#     target_ori = [1, 0, 0, 0]
+#     print(f"Target: {target_pos}")
+#     cur_ori = observation['robot0_eef_quat']
+#     cur_pos = observation['robot0_eef_pos']
+#     print(f"Current: {cur_pos}")
+#     cur_mat = T.quat2mat(cur_ori)
+#     cur_mat = cur_mat @ T.euler2mat(np.array([0, 0, np.pi/4]))
+#     pos_dif = target_pos - cur_pos
+#     print(f"Pose Diff: {pos_dif}")
+#     rot_dif= np.matmul(cur_mat.T, T.get_orientation_error(target_ori, cur_ori))
+#     action = np.hstack([pos_dif, rot_dif, np.array([-1])])  # last number is for the gripper
+#     return action
+
+controller_config= load_controller_config(default_controller="OSC_POSE")
+# controller_config['impedance_mode']='tracking'
 
 # create environment instance
 env = suite.make(
     env_name="PressfitV2", # try with other tasks like "Stack" and "Door"
     robots="Panda",  # try with other robots like "Sawyer" and "Jaco"
     gripper_types="TetrapackGripper",
+    # gripper_types='default',
+    controller_configs= controller_config,
     has_renderer=True,
     has_offscreen_renderer=False,
     use_camera_obs=False,
 )
 
 # reset the environment
-env.reset()
+obs= env.reset()
+done = False
+while True: 
 
-for i in range(1000):
-    action = np.random.randn(env.robots[0].dof) # sample random action
-    obs, reward, done, info = env.step(action)  # take action in the environment
-    env.render()  # render on display
+    if not done:
+        # action = calc_action(obs, env)
+        action = np.array([0.16617313, -0.54680493, -0.23968219, -2.14380896, -0.856529,    3.19363658, -0.05338716])[:6]
+
+        obs, reward, done, info = env.step(action)  # take action in the environment
+        env.render()
+        done = True
+
+    else:
+
+        env.render()  # render on display
